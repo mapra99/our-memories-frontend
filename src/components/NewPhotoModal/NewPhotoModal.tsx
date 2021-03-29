2cadeca88307c885cfaf3952389ff2c9a05a56ea
Miguel Prada <mapra99@gmail.com>
2021-03-28 21:26:58 -0500

    modal logic to upload image and scaffold for post mutation

--- src/components/NewPhotoModal/NewPhotoModal.tsx
+++ src/components/NewPhotoModal/NewPhotoModal.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, SyntheticEvent } from 'react';
 import { Modal } from '../Modal';
 import { ModalTitle } from '../ModalTitle';
 import { InputField } from '../InputField';
@@ -9,26 +10,56 @@ import { NewPhotoModalFormButtons } from './NewPhotoModal.styled';
 
 import { NewPhotoModalProps } from './types';
 
+import { useDirectUpload } from '../../hooks/useDirectUpload';
+
 export const NewPhotoModal = ({ onClose }: NewPhotoModalProps) => {
+  const [name, setName] = useState<string>("");
+  const [file, setFile] = useState<File | null>(null);
+  const { errors: uploadErrors, blob, uploadFile } = useDirectUpload();
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    // TODO: Validate form fields here
+    if (!name || !file) return;
+
+    uploadFile(file)
+  }
+
+  const createPost = () => {
+    // TODO: Build the graphql mutation stuff here to create a new post once the blob was created
+  }
+
+  useEffect(createPost, [blob, uploadErrors])
+
   return (
     <Modal>
       <ModalTitle>
         Add a new photo
       </ModalTitle>
 
-      <form>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <InputGroup>
           <InputLabel>
             Name
           </InputLabel>
-          <InputField type="text" name="post[name]" placeholder="Suspendisse elit massa" />
+          <InputField
+            type="text"
+            name="post[name]"
+            placeholder="Suspendisse elit massa"
+            value={name}
+            onChange={event => setName(event.target.value)} />
         </InputGroup>
 
         <InputGroup>
           <InputLabel>
             File
           </InputLabel>
-          <InputField type="file" name="post[file]" placeholder="Choose a File" />
+          <InputField
+            type="file"
+            name="post[file]"
+            placeholder="Choose a File"
+            accept="image/*"
+            onChange={event => event.target.files && setFile(event.target.files[0]) }/>
         </InputGroup>
 
         <NewPhotoModalFormButtons>
